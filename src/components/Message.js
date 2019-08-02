dc93180bde387ac4a979d6f692f14018c8d5206e
Anthony Shew <anthonyshew@gmail.com>
2019-08-02 14:51:05 -0400

    8/2/2019

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,12 +13,9 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Today, I was going to tell you about how proud I am of you being about to start
-                        your teaching stuff in a classroom - but I don't think I can figure out how!
+                        Today, I just really wanna have sex with you.
                     <br />
-                        I'm honestly so excited for you and I am looking forward to hearing all about it.
-                        I know you'll just be observing but I know you'll be so good all along the whole journey
-                        to a full blown teaching credential!
+                        that is all.
                     <br />
                         Love, Anthony
                 </div>
