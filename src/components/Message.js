d76192e135eb68ac49a7df9d23fa856d330332d0
Anthony Shew <anthonyshew@gmail.com>
2019-08-03 21:58:45 -0400

    8/3/2019

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,9 +13,14 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Today, I just really wanna have sex with you.
+                        Today, I super proud of you for delivering all those pizzas. I know you don't
+                        want to do it and you're doing it because you have to. That's what makes it so
+                        brave and so important
                     <br />
-                        that is all.
+                        Everything we are doing right now is going to pay off in the end. I know it.
+                    <br />
+                        (I fully mean that in the financial sense. I am loving every day that I get to
+                        call you mine and I am proud of both of us for doing what we do every single day.)
                     <br />
                         Love, Anthony
                 </div>
