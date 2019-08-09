0614b4b7d6b97dfe67c338d157c68bbbc0fcee0d
Anthony Shew <anthonyshew@gmail.com>
2019-08-08 22:02:34 -0400

    8/8/2019

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,13 +13,12 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Today, I had a little bit better of a day because they told me they are going to give
-                        me the test that I need. I'm happy for that but I still wish all of this just didn't
-                        have be happening in the first place.
+                        Today, I'm really bored because I don't get to play catch so that's boring, boring, boring.
                         <br />
-                        However, I am thankful for everything that happens to me as far as you being in my life.
-                        You are an absolute gem and every day still I wonder how I got so lucky. Eventually,
-                        though, you're going to have to be sharing that feeling with our kids, just so you know. :)
+                        Someone did mention today, though, that there is less than a month until everyone here breaks
+                        camp and goes him so I plan on leaving for good whenever that happens no matter what. Isn't
+                        that amazing?! Less than a month and I get to see you!!! Yesssssssss!!!
+                        I love you. :)
                     <br />
                         Love, Anthony
                 </div>
