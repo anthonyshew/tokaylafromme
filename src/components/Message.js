6f2599d840eb685374ee53f2f2952821a0c61373
Anthony Shew <anthonyshew@gmail.com>
2019-08-12 22:48:07 -0400

    8/12/2019

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,12 +13,10 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Today, I'm sooooooooooo excited for you to be in one place for awhile. You're
-                        putting it down in the ol' Arizona Zone-a and you get to chill there for a bit
-                        (as far as moving around goes - you still have work to do! :] )
+                        Today, you had your first day in a classroom and I think it is just so cool. You are
+                        destined for such great things and I am just so, so proud of you.
                         <br />
-                        I know achieving even that small amount of stability is amazing for you and
-                        it means a lot for the both of us. One step closer to what we are envisioning!
+                        I love you.
                     <br />
                         Love, Anthony
                 </div>
