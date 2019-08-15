ea397a70ab6505f5347e77e2a714dfc8e00f93d2
Anthony Shew <anthonyshew@gmail.com>
2019-08-14 23:36:05 -0400

    8/14/2019

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,9 +13,8 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Today, I'm gunna be honest, I still am so caught up on how cool you are being in your
-                        little classroom with your little friends and your little notebook and how cute you must
-                        look taking your little notes. Soooooo hot. Want to touch the hiney. AOOGAHHH!
+                        Today, I was super sleepy and fell asleep before I got around to doing this. But I got up
+                        out of bed and made sure that I did this because it is important to me.
                         <br />
                         You cute. :) Me want. :) I love you so. :D
                     <br />
