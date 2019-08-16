3fc41fe61409b7bf49df455bb757c650d5f224fe
Anthony Shew <anthonyshew@gmail.com>
2019-08-16 00:37:23 -0400

    8/16/2019

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,10 +13,11 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Today, I was super sleepy and fell asleep before I got around to doing this. But I got up
-                        out of bed and made sure that I did this because it is important to me.
-                        <br />
-                        You cute. :) Me want. :) I love you so. :D
+                        Today, you had your first "frustrating" day of school and while I know you don't want
+                        to be annoyed, it makes me happy because I know that ya gotta deal with the ups and
+                        downs of anything to truly start to master it, right? So, I'm glad you had a not
+                        perfect one mixed in with all the perfect ones. That gives you a great learning
+                        opportunity and I'm happy you will be better for it in the end. :D
                     <br />
                         Love, Anthony
                 </div>
