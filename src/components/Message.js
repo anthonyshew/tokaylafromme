be71b18c0f2624988355f6c018752811d28f63ec
Anthony Shew <anthonyshew@gmail.com>
2020-05-08 14:57:17 -0700

    5/8/2020.

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        T'was graduation day and it was glorious. There's really nothing else to say about it. :)
+                        Cuddling with you literally has a physiological effect on me, I've noticed. When I'm feeling somewhat stressed or otherwise even moderately unhappy, cuddling with you just sends me to a different mentally that everything is alright. And, of course, if I'm already in the happys, then I just start doing all the better. :D
                     <br />
                         Love, Anthony
                 </div>
