32232b9ac0048492e7a0ccf78b4535c328c9bf9b
Anthony Shew <anthonyshew@gmail.com>
2020-05-07 22:21:23 -0700

    5/7/2020

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        I LOVE watching you do your baking. I think you already know how much I love watching you do something when you're concentrated but, obviously, watching a lady bake in the kitchen really tickles my loins (know you love it when I say that). It makes me think to when you're going to be a momma and I liiiiiiiike it.
+                        T'was graduation day and it was glorious. There's really nothing else to say about it. :)
                     <br />
                         Love, Anthony
                 </div>
