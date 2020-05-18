b25e2491e21a171dfdf8ebaba6392bbd38d6e69b
Anthony Shew <anthonyshew@gmail.com>
2020-05-18 15:42:26 -0700

    5/18/2020

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Kayla. Great. Freaking. Cake.
+                        Oh my goodness, I haven't seen you for like 24 hours I MISS YOU WHAT THE HECK
                     <br />
                         Love, Anthony
                 </div>
