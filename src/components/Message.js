a917e27e69f6688cd1f1745d74bd86331eb5dd86
Anthony Shew <anthonyshew@gmail.com>
2020-06-11 14:54:27 -0700

    6/11/2020

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Are we gunna go volley to the ball?
+                        Yes, I know my poop is sitting directly on the spoon that I am using but I've built up an immunity to it. Just like the 16 year old soup that I've been eating for the past year. You see, I'm perfectly okay. Nevermind the fact that I'm obese and have a bladder infection, a swollen stomach, mold growing under my right foot, IBS, TPS, LMS, vitamin deficencies, and that my hair is stiffer than copper wire. The hair thing is just because I can't get to my shower since that's where my motorcycle has been parked since 1978. It's a vintage piece, you wouldn't understand. HOARDERSSSSSSSSSS
                     <br />
                         Love, Anthony
                 </div>
