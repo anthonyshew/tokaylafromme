f1a4815274f7de5351070d66151bf3874715b908
Anthony Shew <anthonyshew@gmail.com>
2020-07-27 10:19:34 -0700

    7/27/2020

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        OH. MY. GOD. MRS. SHEW OFFICIAL. AND HOUSE KEYS. ONE DAY. BIG DAY. HUGE DAY. HUGE. WOW.
+                        Hey. Wanna go live in our house? Yeah, me too.
                         <br />
                         Love, Anthony Edward Shew
                 </div>
