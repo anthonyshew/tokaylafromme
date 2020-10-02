f5d43e0699a9fd36992b379d3be5041afd010a66
Anthony Shew <anthonyshew@gmail.com>
2020-10-02 14:24:27 -0700

    10/20/2020

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Big, strong, meaty, exquisitely dressed, intelligent, hard-working Kayla vs. long week ahead. Who will win this dramatic showdown?!
+                        You killed it for a full, long week, lover dover. Have a great nap today. :)
                         <br />
                         Love, Anthony Edward Shew
                 </div>
