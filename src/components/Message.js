4b0030a3b5ceaeca110d362f8e53ae42f42316ca
Anthony Shew <anthonyshew@gmail.com>
2020-11-09 11:02:50 -0700

    11/9/2020

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        You so cute in ya little pants wit ya long sleeve shirt and ya cookies. :)
+                        BIG week for you this week! HUGE! No volleyball practices? More time for sleeping and hanging out with me? WAHOOOOOO!
                         <br />
                         Love, Anthony Edward Shew
                 </div>
