efb5cc205d3d41a1d0d99caad19f1288e8fe6164
Anthony Shew <anthonyshew@gmail.com>
2021-02-23 15:59:58 -0700

    2/23/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Baking and cooking toys, yeeeeee!
+                        I love you for grinding your way through the body today for the benefit of the children.
                         <br />
                         Love, Anthony Edward Shew
                 </div>
