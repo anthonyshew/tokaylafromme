b3e787301a0f04d9ead4b8d0d21e6929ccc8d661
Anthony Shew <anthonyshew@gmail.com>
2021-05-17 15:01:04 -0700

    5/17/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        AH MY GAD JUST ONE MORE WEEK WAT
+                        ALAST WEEK BEBEHHHHH
                         <br />
                         Love, Anthony Edward Shew
                 </div>
