b7d686c7833d0fa177b8825029d6b98ccf4b1977
Anthony Shew <anthonyshew@gmail.com>
2021-08-19 13:18:13 -0700

    8/19/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Wednesday? More like one more schoolday after this one. Yup.
+                        What do you want for your birthday? :)
                         <br />
                         Love, Anthony Edward Shew
                     </div>
