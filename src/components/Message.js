3ad478dbaf170182a26976c95424d23388571d07
Anthony Shew <anthonyshew@gmail.com>
2021-08-24 15:01:13 -0700

    8/24/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Are ya ready for this week of school, kids?! Aye aye, captainnnn!
+                        Volleyball season coming up and we are excitedddd!
                         <br />
                         Love, Anthony Edward Shew
                     </div>
