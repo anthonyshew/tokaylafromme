e199d3890aa4dc012415b0093e2a39529d1ae454
Anthony Shew <anthonyshew@gmail.com>
2021-10-12 23:18:30 -0700

    10/12/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        All hail the Z-Pak!
+                        It is always so nice to see you in your element at da volleyballs. Totally adorbs.
                         <br />
                         Love, Anthony Edward Shew
                     </div>
