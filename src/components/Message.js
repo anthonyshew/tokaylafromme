c4c17bf172f65e55a23dd4fae1f8d67c2caca07e
Anthony Shew <anthonyshew@gmail.com>
2021-11-14 18:22:44 -0700

    11/14/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,7 +13,7 @@ class Message extends Component {
             >
                 <div className="message-container" onClick={this.props.openMessage}>
                     <div className="message-body">
-                        Biiiiiiig hockey night in the Shew household.
+                        Thanks for dinner, babe. :)
                         <br />
                         Love, Anthony Edward Shew
                     </div>
