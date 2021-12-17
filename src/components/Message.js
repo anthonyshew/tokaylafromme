8a51e3188ebce52bf053cf68d2db300aea8157df
Anthony Shew <anthonyshew@gmail.com>
2021-12-17 15:01:11 -0700

    12/17/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition } from "react-transition-group";
 
 class Message extends Component {
-    render() {
-        return (
-            <CSSTransition
-                in={true}
-                appear={true}
-                timeout={600}
-                classNames="fadeIn"
-            >
-                <div className="message-container" onClick={this.props.openMessage}>
-                    <div className="message-body">
-                        So happy for you and your new job. From one thing you are crazy good at to another. Too proud of a hubby over here. :)
-                        <br />
-                        Love, Anthony Edward Shew
-                    </div>
-                </div>
-            </CSSTransition>
-        )
-    }
+  render() {
+    return (
+      <CSSTransition in={true} appear={true} timeout={600} classNames="fadeIn">
+        <div className="message-container" onClick={this.props.openMessage}>
+          <div className="message-body">
+            You are such a good little floral lady. They ain't wrong...And
+            already on day 4?!
+            <br />
+            Love, Anthony Edward Shew
+          </div>
+        </div>
+      </CSSTransition>
+    );
+  }
 }
 
 export default Message;
