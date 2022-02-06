3cc4287c594429d347d09cfd5cd30a3908eb5555
Anthony Shew <anthonyshew@gmail.com>
2022-02-06 15:01:11 -0700

    2/6/2021

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ class Message extends Component {
       <CSSTransition in={true} appear={true} timeout={600} classNames="fadeIn">
         <div className="message-container" onClick={this.props.openMessage}>
           <div className="message-body">
-            So glad to have you back. :)
+            HOW BOUT THEM PRS THOUGH
             <br />
             Love, Anthony Edward Shew
           </div>
