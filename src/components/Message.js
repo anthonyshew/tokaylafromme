03aebddeb90850b7451e0f59897465b6ae2f2933
Anthony Shew <anthonyshew@gmail.com>
2022-02-07 15:01:41 -0700

    2/7/2022

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ class Message extends Component {
       <CSSTransition in={true} appear={true} timeout={600} classNames="fadeIn">
         <div className="message-container" onClick={this.props.openMessage}>
           <div className="message-body">
-            HOW BOUT THEM PRS THOUGH
+            Do you like lollipop sticks?
             <br />
             Love, Anthony Edward Shew
           </div>
