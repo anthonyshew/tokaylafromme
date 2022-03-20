a442189e919cef199fc74d76ca6cdadd4f4b7e08
Anthony Shew <anthonyshew@gmail.com>
2022-03-20 15:01:12 -0700

    3/20/2022

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ class Message extends Component {
       <CSSTransition in={true} appear={true} timeout={600} classNames="fadeIn">
         <div className="message-container" onClick={this.props.openMessage}>
           <div className="message-body">
-            IM OUT HERE MAKING BABIES...BY MYSELF
+            BAKELAAAAA on the looooooose!
             <br />
             Love, Anthony Edward Shew
           </div>
