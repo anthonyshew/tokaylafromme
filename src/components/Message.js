dff9f2299507b3a16fab214eb87767d577e612b7
Anthony Shew <anthonyshew@gmail.com>
2022-03-29 15:03:28 -0700

    3/29/2022

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,8 +8,8 @@ class Message extends Component {
       <CSSTransition in={true} appear={true} timeout={600} classNames="fadeIn">
         <div className="message-container" onClick={this.props.openMessage}>
           <div className="message-body">
-            How many Kaylas does it take to make an Anthony happy? One, just the
-            one actually, 'cause she bomb.
+            I swear, one of these days all my baby making will pay off and I
+            will be ecstatic.
             <br />
             Love, Anthony Edward Shew
           </div>
