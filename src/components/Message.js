a2b8d3c76e065f517ec2a8e43e9104197f5fb7f1
Anthony Shew <anthonyshew@gmail.com>
2022-04-05 15:01:14 -0700

    4/5/2022

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ class Message extends Component {
       <CSSTransition in={true} appear={true} timeout={600} classNames="fadeIn">
         <div className="message-container" onClick={this.props.openMessage}>
           <div className="message-body">
-            Working on the weekend? You? VIBES!!!
+            Ain't working for today, it's for tomorrow.
             <br />
             Love, Anthony Edward Shew
           </div>
